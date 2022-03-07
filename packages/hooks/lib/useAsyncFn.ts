1efef91ca7f31172d0e91f05e7e6529d0c8c3ea3
liuhao <liuhao@taou.com>
2022-03-07 16:28:44 +0800

    feat: test microbundle

--- packages/hooks/lib/useAsyncFn.ts
+++ packages/hooks/lib/useAsyncFn.ts
@@ -1,10 +1,11 @@
 import { useImmer } from './useImmer';
 export function useAsycFn(fn, deps = [], initialState = {loading: false}) {
+    console.log('ddd')
     // const [state, setState] = useImmer(initialState);
     // const hooksDeps = [fn, isMounted, state.loading];
     // const callback = () => {
     //     fn()
     // }
     // return [state, callback];
-    
+
 }
