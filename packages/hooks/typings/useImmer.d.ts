54d3088de0c9967c56a98f96c1bb5abde2252990
liuhao <liuhao@taou.com>
2022-03-07 16:24:33 +0800

    feat: test microbundle

--- packages/hooks/typings/useImmer.d.ts
+++
@@ -1,6 +0,0 @@
-import { Draft } from "immer";
-export declare type DraftFunction<S> = (draft: Draft<S>) => void;
-export declare type Updater<S> = (update: S | DraftFunction<S>) => void;
-export declare type ImmerHooks<S> = [S, Updater<S>];
-export declare function useImmer<S = unknown>(initValue: S | (() => S)): ImmerHooks<S>;
-//# sourceMappingURL=useImmer.d.ts.map
